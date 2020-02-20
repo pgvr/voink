18424355ff5ada99ffeff2c1051227a224ea3426
Patrick Göler von Ravensburg <mail@pgvr.dev>
2020-02-20 20:48:30 +0100

    add proper formatting and linting

--- voink-frontend/src/app/app.component.ts
+++ voink-frontend/src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core"
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: "app-root",
+    templateUrl: "./app.component.html",
+    styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'voink-frontend';
+    title = "voink-frontend"
+
+    testFunc() {
+        const test = 0
+        console.log("Hi")
+    }
 }
