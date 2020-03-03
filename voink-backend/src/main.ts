5c5cd5881b308cc9e57f93bc21ac567f2d38bec3
Patrick Göler von Ravensburg <mail@pgvr.dev>
2020-03-03 21:08:29 +0100

    add proxy to backend

--- voink-backend/src/main.ts
+++ voink-backend/src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app.module"
+import * as proxy from "http-proxy-middleware"
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     app.enableCors()
+    app.use(
+        "/proxy",
+        proxy.createProxyMiddleware({
+            pathRewrite: {
+                "^/proxy/": "/",
+            },
+            target: "https://vod-metro.twitch.tv",
+            secure: false,
+            changeOrigin: true,
+        }),
+    )
     const port = process.env.PORT || 3000
     await app.listen(port)
 }
